e9be1fece06b6303df33162542e34e193cb0a2d1
Muhammadsid2011 <muhammadsid2011@gmail.com>
2024-12-18 19:02:31 +0500

    Update main.py

--- 33-Day-33-Dictionary/main.py
+++ 33-Day-33-Dictionary/main.py
@@ -1,10 +1,10 @@
 info = {'name':'Karan', 'age':19, 'eligible':True}
-# print(info) 
-# print(info.keys())
-# print(info.values())
+print(info) 
+print(info.keys())
+print(info.values())
 
-# for key in info.keys():
-#   print(f"The value corresponding to the key {key} is {info[key]}")
+for key in info.keys():
+   print(f"The value corresponding to the key {key} is {info[key]}")
 
 print(info.items())
 for key, value in info.items():
